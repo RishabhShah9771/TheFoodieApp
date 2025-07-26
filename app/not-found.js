8593a826785aec92c0120bd102b670a0655defd7
RISHABH SHAH <rnshah1997@gmail.com>
2025-07-26 18:12:06 -0400

    ImageUpdate

--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <main className="not-found">
-      <h1>Not Found</h1>
-      <p>Unable to find the requested page or resource.</p>
+      <h1>Meal Not Found</h1>
+      <p>Unable to find the requested page.</p>
     </main>
   );
 }

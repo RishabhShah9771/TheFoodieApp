1c18d19c705b0c459df8bb12ab02e60bdad2cc58
RISHABH SHAH <rnshah1997@gmail.com>
2025-07-26 17:35:13 -0400

    meal update1

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,4 +1,33 @@
-const MealsDetail = () => {
-  return <h1> MealsDetail</h1>;
-};
-export default MealsDetail;
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+
+export default async function MealsDetail({ params }) {
+  const meal = await getMeal(params.mealSlug);
+  console.log(meal);
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image alt="" fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: "...",
+          }}
+        ></p>
+      </main>
+    </>
+  );
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,14 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getAllMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getAllMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 async function MealsPage() {
-  const meals = await getAllMeals();
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +26,11 @@ async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
